c5c43e4a05271a9edd8d6ef3340206f82e06550d
Abdul Qadeer <abdul@cidrusmoney.com>
2023-11-28 19:13:35 +0500

    login form created.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,5 +7,30 @@
 </head>
 <body>
     <h1>New branch</h1>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="">
+                            <div class="form-group">
+                                <label for="">Username</label>
+                                <input type="text" class="form-control" name="username">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Password</label>
+                                <input type="text" class="form-control" name="username">
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
